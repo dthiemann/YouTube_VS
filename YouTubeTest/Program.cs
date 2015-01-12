cb0bd094ecf66429302f374fced3360acdf915f6
Dylan Thiemann <dylanthiemann@gmail.com>
2015-01-12 14:38:30 -0600

    added references to the Google YouTube apis

--- YouTubeTest/Program.cs
+++ YouTubeTest/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Google Api
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
 namespace YouTubeTest {
     class Program {
         static void Main(string[] args) {
